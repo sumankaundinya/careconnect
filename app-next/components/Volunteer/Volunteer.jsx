be56e04779e71c5d9a3f378fba827f5412314c0f
Suman J <sumankaundinya@gmail.com>
2025-09-14 15:47:06 +0200

    Update Volunteer.jsx

--- app-next/components/Volunteer/Volunteer.jsx
+++ app-next/components/Volunteer/Volunteer.jsx
@@ -21,12 +21,18 @@ const Volunteer = ({ volunteer }) => {
 
       <div className={styles.content}>
         <h3 className={styles.name}>{volunteer.name}</h3>
-        <p className={styles.address}>Address: {volunteer.address}</p>
-        <p>Email: {volunteer.email}</p>
-        <p>Phone: {volunteer.phone_nr}</p>
-        <p>Gender: {volunteer.gender}</p>
-        <p>Routine: {volunteer.routine || "Not specified"}</p>
-
+        <p className={styles.services}>
+          Services:{" "}
+          {volunteer.services?.length
+            ? volunteer.services.join(", ")
+            : "Not specified"}
+        </p>
+        <p className={styles.availability}>
+          Availability:{" "}
+          {volunteer.availability?.length
+            ? volunteer.availability.join(", ")
+            : "Not specified"}
+        </p>
         <Link
           href={`/volunteers/${volunteer.id}`}
           className={styles.detailsButton}
