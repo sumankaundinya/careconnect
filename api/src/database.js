9d39c49a322826b3c6c1774bbbbc18b7b6ba4db8
Suman J <sumankaundinya@gmail.com>
2025-09-14 15:10:25 +0200

    Update database.js

--- api/src/database.js
+++ api/src/database.js
@@ -3,11 +3,11 @@ import knex from "knex";
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.PGHOST || "localhost",
-    user: process.env.PGUSER || "postgres",
-    password: process.env.PGPASSWORD || "Shanaya@402",
-    database: process.env.PGDATABASE || "careconnect",
-    port: Number(process.env.PGPORT) || 5432,
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    port: Number(process.env.PGPORT),
   },
   pool: { min: 0, max: 10 },
 });
