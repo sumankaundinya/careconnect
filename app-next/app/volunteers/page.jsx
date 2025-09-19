6d64e55ced3125b2c4f74ffd061523e791574ee9
sumankaundinya <sumankaundinya@gmail.com>
2025-09-19 12:01:34 +0200

    refactored the build errors inorder to deploy on netlify

--- app-next/app/volunteers/page.jsx
+++ app-next/app/volunteers/page.jsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { Suspense } from "react";
 import VolunteerList from "@/components/VolunteerList/VolunteerList";
 import styles from "../../components/VolunteerList/VolunteerList.module.css";
 
 export default function VolunteersPage() {
   return (
     <div className={styles.container}>
-      <VolunteerList />
+      <Suspense fallback={<div>Loading volunteers...</div>}>
+        <VolunteerList />
+      </Suspense>
     </div>
   );
 }
